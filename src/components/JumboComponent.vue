973d42b2f732a206903bf45d8a7f8687242b35ed
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-04-11 21:57:30 +0200

    nudge foto landing

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -1,49 +1,46 @@
 <script>
-import {store} from '../data/store'
-export default {
-  name: 'JumboComponent',
-  props: {
-    landingImg: String,
-    artistName: String,
-    isMobile: Boolean
-  },
+  import { store } from "../data/store";
+  export default {
+    name: "JumboComponent",
+    props: {
+      landingImg: String,
+      artistName: String,
+      isMobile: Boolean
+    },
 
-  data(){
-    return {
-      store,
-    }
-  },
-  methods:{
-
-  }
-}
+    data() {
+      return {
+        store
+      };
+    },
+    methods: {}
+  };
 </script>
 
 <template>
-
   <div class="em-container">
-
-      <img :src="landingImg" :alt="artistName" :class="{'nudge' : isMobile}">
-
+    <img
+      :src="landingImg"
+      :alt="artistName"
+      :class="{ nudge: isMobile }" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use '../styles/partials/mixin' as *;
-@use '../styles/partials/variables' as *;
+  @use "../styles/partials/mixin" as *;
+  @use "../styles/partials/variables" as *;
 
-.em-container {
- height: calc(100vh - $header-height);
- margin-top: $header-height;
- img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
- }
-}
+  .em-container {
+    height: calc(100vh - $header-height);
+    margin-top: $header-height;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 
-.nudge {
-  object-position: -65px 0;
-}
-
+  .nudge {
+    object-position: -365px 0;
+  }
 </style>
