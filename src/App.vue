2cb6ab7f5a65c3d39075112124ee8fe495dbedbd
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-04-11 21:19:48 +0200

    cambiato foto jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,65 @@
 <script>
-import HeaderComponentVue from './components/HeaderComponent.vue';
-import JumboComponentVue from './components/JumboComponent.vue';
-import BioComponentVue from './components/BioComponent.vue';
-import PhotoGalleryComponentVue from './components/PhotoGalleryComponent.vue';
-import SoundCloudComponentVue from './components/SoundCloudComponent.vue';
-import GigsComponentVue from './components/GigsComponent.vue';
-import ComingSoonComponentVue from './components/ComingSoonComponent.vue';
-import FooterComponentVue from './components/FooterComponent.vue';
-import {store} from '../src/data/store'
-export default {
-  name: 'App',
-  components: {
-    HeaderComponentVue,
-    JumboComponentVue,
-    BioComponentVue,
-    PhotoGalleryComponentVue,
-    SoundCloudComponentVue,
-    GigsComponentVue,
-    ComingSoonComponentVue,
-    FooterComponentVue
-  },
-  data(){
-    return {
-      store,
-      isMobile: false
-    }
-  },
+  import HeaderComponentVue from "./components/HeaderComponent.vue";
+  import JumboComponentVue from "./components/JumboComponent.vue";
+  import BioComponentVue from "./components/BioComponent.vue";
+  import PhotoGalleryComponentVue from "./components/PhotoGalleryComponent.vue";
+  import SoundCloudComponentVue from "./components/SoundCloudComponent.vue";
+  import GigsComponentVue from "./components/GigsComponent.vue";
+  import ComingSoonComponentVue from "./components/ComingSoonComponent.vue";
+  import FooterComponentVue from "./components/FooterComponent.vue";
+  import { store } from "../src/data/store";
+  export default {
+    name: "App",
+    components: {
+      HeaderComponentVue,
+      JumboComponentVue,
+      BioComponentVue,
+      PhotoGalleryComponentVue,
+      SoundCloudComponentVue,
+      GigsComponentVue,
+      ComingSoonComponentVue,
+      FooterComponentVue
+    },
+    data() {
+      return {
+        store,
+        isMobile: false
+      };
+    },
 
-  computed: {
-    photoToggle(){
-      let imgPath = ''
-      this.isMobile? imgPath = '/Media/WA-img/landing-photo.jpeg' : imgPath = '/Media/WA-img/AlterantiveLandingImg.jpeg'
-      return imgPath
-    }
-  },
+    computed: {
+      photoToggle() {
+        let imgPath = "";
+        this.isMobile
+          ? (imgPath = "/Media/WA-img/landing-photo.jpeg")
+          : (imgPath = "/Media/WA-img/foto_inziale.jpeg");
+        return imgPath;
+      }
+    },
 
-  mounted(){
-    store.screenWidth = window.screen.width
-    if(store.screenWidth < 1200) this.isMobile = true
-  }
-}
+    mounted() {
+      store.screenWidth = window.screen.width;
+      if (store.screenWidth < 1200) this.isMobile = true;
+    }
+  };
 </script>
 
 <template>
-  
-<HeaderComponentVue artistName="Alice Caronna"/>
+  <HeaderComponentVue artistName="Alice Caronna" />
 
-<JumboComponentVue artistName="Alice Caronna" 
-:landingImg="photoToggle" :isMobile="this.isMobile"/>
+  <JumboComponentVue
+    artistName="Alice Caronna"
+    :landingImg="photoToggle"
+    :isMobile="this.isMobile" />
 
-
-<BioComponentVue :isMobile="this.isMobile"/>
-<PhotoGalleryComponentVue :isMobile="this.isMobile"/>
-<SoundCloudComponentVue :isMobile="this.isMobile"/>
-<GigsComponentVue :isMobile="this.isMobile"/>
-<ComingSoonComponentVue :isMobile="this.isMobile"/>
-<FooterComponentVue/>
-
-
+  <BioComponentVue :isMobile="this.isMobile" />
+  <PhotoGalleryComponentVue :isMobile="this.isMobile" />
+  <SoundCloudComponentVue :isMobile="this.isMobile" />
+  <GigsComponentVue :isMobile="this.isMobile" />
+  <ComingSoonComponentVue :isMobile="this.isMobile" />
+  <FooterComponentVue />
 </template>
 
-
-
-
 <style lang="scss">
-@use './styles/general.scss' as*;
-
-
+  @use "./styles/general.scss" as *;
 </style>
