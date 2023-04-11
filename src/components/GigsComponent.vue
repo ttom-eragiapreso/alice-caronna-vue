d00e3d0a6610c6b058f481d84ecb4c468fedcb2d
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-04-11 22:15:34 +0200

    video youtube

--- src/components/GigsComponent.vue
+++ src/components/GigsComponent.vue
@@ -1,78 +1,100 @@
 <script>
-import {store} from '../data/store'
-import { Swiper, SwiperSlide } from "swiper/vue";
+  import { store } from "../data/store";
+  import { Swiper, SwiperSlide } from "swiper/vue";
 
-// Import Swiper styles
-import "swiper/css";
+  // Import Swiper styles
+  import "swiper/css";
 
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+  import "swiper/css/pagination";
+  import "swiper/css/navigation";
 
-// import "/style.css";
+  // import "/style.css";
 
-// import required modules
-import { Pagination, Navigation, Autoplay } from "swiper";
-export default {
-  name: 'GigsComponent',
-  props: {
-    isMobile: Boolean
-  },
-  data(){
-    return {
-      store
+  // import required modules
+  import { Pagination, Navigation, Autoplay } from "swiper";
+  export default {
+    name: "GigsComponent",
+    props: {
+      isMobile: Boolean
+    },
+    data() {
+      return {
+        store
+      };
+    },
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    setup() {
+      return {
+        modules: [Pagination, Navigation, Autoplay]
+      };
     }
-  },
-  components: {
-    Swiper,
-    SwiperSlide
-  },
-  setup() {
-    return {
-      modules: [Pagination, Navigation, Autoplay],
-    };
-  },
-}
+  };
 </script>
 
 <template>
-
-<div class="em-container" id="vi">
+  <div
+    class="em-container"
+    id="vi">
     <swiper
-    :pagination="{
-      dynamicBullets: true,
-    }"
-    :modules="modules"
-    :navigation="!isMobile"
-    class="mySwiper"
-    id="swiper"
-  >
-    <swiper-slide v-for="(video, index) in store.gigsVideos" :key="index">
-      <video :src="video.url" controls :autoplay="isMobile"></video>
-    </swiper-slide>
-  </swiper>
+      :pagination="{
+        dynamicBullets: true
+      }"
+      :modules="modules"
+      :navigation="!isMobile"
+      class="mySwiper"
+      id="swiper">
+      <swiper-slide>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/chh_EIuONpw"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
+      </swiper-slide>
+      <swiper-slide>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/mS_6ojtCWuI"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
+      </swiper-slide>
+      <swiper-slide
+        v-for="(video, index) in store.gigsVideos"
+        :key="index">
+        <video
+          :src="video.url"
+          controls
+          :autoplay="isMobile"></video>
+      </swiper-slide>
+    </swiper>
   </div>
-
-
 </template>
 
 <style lang="scss" scoped>
-@use '../styles/partials/mixin' as *;
-@use '../styles/partials/variables' as *;
+  @use "../styles/partials/mixin" as *;
+  @use "../styles/partials/variables" as *;
 
-.em-container {
-  height: 80vh;
-  background-color: #333;
-  padding: 1.5rem 0;
-}
+  .em-container {
+    height: 80vh;
+    background-color: #333;
+    padding: 1.5rem 0;
+  }
 
-#swiper {
-  height: 100%;
-}
+  #swiper {
+    height: 100%;
+  }
 
-video {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
 </style>
